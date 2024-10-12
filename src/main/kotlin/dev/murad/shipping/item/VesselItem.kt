e1c6af8728ddb7329c1695d58d5b6e64e70cc842
DESKTOP-QLS9EQQ\Nutzer <d>
2024-10-12 14:53:28 +0200

    cleanup

--- src/main/kotlin/dev/murad/shipping/item/VesselItem.kt
+++ src/main/kotlin/dev/murad/shipping/item/VesselItem.kt
@@ -20,52 +20,56 @@ class VesselItem(props: Properties, private val addEntity: AddEntityFunction) : 
     }
 
     override fun use(world: Level, player: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
+
         val itemstack = player.getItemInHand(hand)
         val hitResult = getPlayerPOVHitResult(world, player, ClipContext.Fluid.ANY)
         if (hitResult.type == HitResult.Type.MISS) {
             return InteractionResultHolder.pass(itemstack)
-        } else {
-            val vector3d = player.getViewVector(1.0f)
-            val list = world.getEntities(player, player.boundingBox.expandTowards(vector3d.scale(5.0)).inflate(1.0),
-                EntitySelector.NO_SPECTATORS.and { obj: Entity -> obj.isPickable })
-            if (list.isNotEmpty()) {
-                val vector3d1 = player.getEyePosition(1.0f)
+        }
 
-                for (entity in list) {
-                    val axisalignedbb = entity.boundingBox.inflate(entity.pickRadius.toDouble())
-                    if (axisalignedbb.contains(vector3d1)) {
-                        return InteractionResultHolder.pass(itemstack)
-                    }
+        val vector3d = player.getViewVector(1.0f)
+        val entities = world.getEntities(
+            player, player.boundingBox.expandTowards(vector3d.scale(5.0)).inflate(1.0),
+            EntitySelector.NO_SPECTATORS.and { obj: Entity -> obj.isPickable })
+        if (entities.isNotEmpty()) {
+            val vector3d1 = player.getEyePosition(1.0f)
+
+            for (entity in entities) {
+                val axisalignedBox = entity.boundingBox.inflate(entity.pickRadius.toDouble())
+                if (axisalignedBox.contains(vector3d1)) {
+                    return InteractionResultHolder.pass(itemstack)
                 }
-            }
-
-            if (hitResult.type == HitResult.Type.BLOCK) {
-                val entity = getEntity(world, itemstack, hitResult)
-                entity.yRot = player.yRot
-                if (!world.noCollision(entity, entity.boundingBox.inflate(-0.1))) {
-                    return InteractionResultHolder.fail(itemstack)
-                } else {
-                    if (!world.isClientSide) {
-                        world.addFreshEntity(entity)
-                        if (!player.abilities.instabuild) {
-                            itemstack.shrink(1)
-                        }
-                    }
-
-                    player.awardStat(Stats.ITEM_USED[this])
-                    return InteractionResultHolder.sidedSuccess(itemstack, world.isClientSide())
-                }
-            } else {
-                return InteractionResultHolder.pass(itemstack)
             }
         }
+
+        if (hitResult.type == HitResult.Type.BLOCK) {
+            val entity = getEntity(world, itemstack, hitResult)
+            entity.yRot = player.yRot
+
+            return if (!world.noCollision(entity, entity.boundingBox.inflate(-0.1))) {
+                InteractionResultHolder.fail(itemstack)
+            } else {
+                if (!world.isClientSide) {
+                    world.addFreshEntity(entity)
+                    if (!player.abilities.instabuild) {
+                        itemstack.shrink(1)
+                    }
+                }
+
+                player.awardStat(Stats.ITEM_USED[this])
+                InteractionResultHolder.sidedSuccess(itemstack, world.isClientSide())
+            }
+        }
+
+        return InteractionResultHolder.pass(itemstack)
     }
 
-    protected fun getEntity(world: Level, stack: ItemStack, raytraceresult: BlockHitResult): Entity {
-        val e = addEntity.apply(world, raytraceresult.location.x, raytraceresult.location.y, raytraceresult.location.z)
-        if (!stack.hoverName.string.isBlank()) {
-            e.customName = stack.hoverName
+    private fun getEntity(world: Level, stack: ItemStack, hitResult: BlockHitResult): Entity {
+
+        val entity = addEntity.apply(world, hitResult.location.x, hitResult.location.y, hitResult.location.z)
+        if (stack.hoverName.string.isNotBlank()) {
+            entity.customName = stack.hoverName
         }
-        return e
+        return entity
     }
 }
