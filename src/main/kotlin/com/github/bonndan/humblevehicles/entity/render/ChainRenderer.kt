5519cf8555bb0614bcf3469e67c2300733955644
DESKTOP-QLS9EQQ\Nutzer <d>
2024-12-29 14:58:22 +0100

    fix chain height

--- src/main/kotlin/com/github/bonndan/humblevehicles/entity/render/ChainRenderer.kt
+++ src/main/kotlin/com/github/bonndan/humblevehicles/entity/render/ChainRenderer.kt
@@ -13,38 +13,35 @@ import kotlin.math.atan2
 import kotlin.math.asin
 import kotlin.math.ceil
 
-private const val CHAIN_Y_OFFSET = 0.0
+private const val CHAIN_Y_OFFSET = 0.4
 
 class ChainRenderer(context: EntityRendererProvider.Context) {
 
     private val chainModel: ChainModel = ChainModel(context.bakeLayer(ChainModel.Companion.LAYER_LOCATION))
-    private val CHAIN_TEXTURE = ResourceLocation.fromNamespaceAndPath(MOD_ID, "textures/entity/chain.png")
+    private val chainTexture = ResourceLocation.fromNamespaceAndPath(MOD_ID, "textures/entity/chain.png")
 
     fun getAndRenderChain(
         pos: Vec3,
         followerPos: Vec3,
         matrixStack: PoseStack,
         buffer: MultiBufferSource,
-        p_225623_6_: Int
+        packedLight: Int
     ) {
-
-        val from = pos.add(0.0, CHAIN_Y_OFFSET, 0.0)
-        val to = followerPos.add(0.0, CHAIN_Y_OFFSET, 0.0)
-
-        matrixStack.pushPose()
-        val vec = from.vectorTo(to)
+        val vec = pos.vectorTo(followerPos)
         val dist = vec.length()
         val segments = ceil(dist * 4).toInt()
 
-        // TODO: fix pitch
+        matrixStack.pushPose()
         matrixStack.mulPose(Axis.YP.rotation(-atan2(vec.z, vec.x).toFloat()))
         matrixStack.mulPose(Axis.ZP.rotation((asin(vec.y / dist)).toFloat()))
+        matrixStack.translate(0.0, CHAIN_Y_OFFSET, 0.0)
         matrixStack.pushPose()
-        val ivertexbuilderChain = buffer.getBuffer(chainModel.renderType(CHAIN_TEXTURE))
+
+        val ivertexbuilderChain = buffer.getBuffer(chainModel.renderType(chainTexture))
         for (i in 1 until segments) {
             matrixStack.pushPose()
             matrixStack.translate(i / 4.0, 0.0, 0.0)
-            chainModel.renderToBuffer(matrixStack, ivertexbuilderChain, p_225623_6_, OverlayTexture.NO_OVERLAY)
+            chainModel.renderToBuffer(matrixStack, ivertexbuilderChain, packedLight, OverlayTexture.NO_OVERLAY)
             matrixStack.popPose()
         }
 
